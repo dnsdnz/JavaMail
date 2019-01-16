be697c133416fa0b68c511c60f869b36805cce9d
Deniz Cakmak <bdeniz.cakmak@std.hku.edu.tr>
2019-01-16 20:37:27 +0300

    Add files via upload
    first commit

---
+++ SendEmail.java
@@ -0,0 +1,66 @@
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class SendEmail {
+
+	Properties emailProperties;
+	Session mailSession;
+	MimeMessage emailMessage;
+
+	public static void main(String args[]) throws AddressException,	MessagingException {
+
+		SendEmail javaEmail = new SendEmail();
+
+		javaEmail.setMailServerProperties();
+		javaEmail.createEmailMessage();
+		javaEmail.sendEmail();
+	}
+
+	public void setMailServerProperties() {
+
+		String emailPort = "587";      //gmail's smtp port
+
+		emailProperties = System.getProperties();
+		emailProperties.put("mail.smtp.port", emailPort);
+		emailProperties.put("mail.smtp.auth", "true");
+		emailProperties.put("mail.smtp.starttls.enable", "true");
+
+	}
+
+	public void createEmailMessage() throws AddressException,MessagingException {
+		
+		String[] toEmails = {"Addresses of recipients" };
+		String emailSubject = "Subject of emil";
+		String emailBody = "This is an email sent test making with Java api.";
+
+		mailSession = Session.getDefaultInstance(emailProperties, null);
+		emailMessage = new MimeMessage(mailSession);
+
+		for (int i = 0; i < toEmails.length; i++) {
+			emailMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(toEmails[i]));
+		}
+
+		emailMessage.setSubject(emailSubject);
+		emailMessage.setContent(emailBody, "text/html");
+	}
+
+	public void sendEmail() throws AddressException, MessagingException {
+
+		String emailHost = "smtp.gmail.com";
+		String fromUser = "yourid@gmail.com";
+		String fromUserEmailPassword = "password";
+
+		Transport transport = mailSession.getTransport("smtp");
+
+		transport.connect(emailHost, fromUser, fromUserEmailPassword);
+		transport.sendMessage(emailMessage, emailMessage.getAllRecipients());
+		transport.close();
+		System.out.println("Email sent successfully.");
+	}
+}
